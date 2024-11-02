a14fabaaf7f9ee845f1adabdffa5f4a4ebad4b1d
liuhuoz <liuhuo2012@gmail.com>
2024-11-02 17:13:31 +0800

    add dataset doc

---
+++ R/data.R
@@ -0,0 +1,39 @@
+#' Ligand-receptor interactions in CellChat database for human
+#'
+#' The ligand-receptor interaction database curated in CellChat tool
+#'
+#' @format A data.frame includes the ligand-receptor interactions
+#' @source \url{https://github.com/sqjin/CellChat/}
+"cellchat_human"
+
+#' Ligand-receptor interactions in CellChat database for mouse
+#'
+#' The ligand-receptor interaction database curated in CellChat tool
+#'
+#' @format A data.frame includes the ligand-receptor interactions
+#' @source \url{https://github.com/sqjin/CellChat/}
+"cellchat_mouse"
+
+#' Ligand-receptor interactions in CellChat database for zebrafish
+#'
+#' The ligand-receptor interaction database curated in CellChat tool
+#'
+#' @format A data.frame includes the ligand-receptor interactions
+#' @source \url{https://github.com/sqjin/CellChat/}
+"cellchat_zebrafish"
+
+#' Ligand-receptor interactions in CellphoneDB database for human
+#'
+#' The ligand-receptor interaction database curated in CellphoneDB
+#'
+#' @format A data.frame includes the ligand-receptor interactions
+#' @source \url{https://www.cellphonedb.org/}
+"cpdb_human"
+
+#' Ligand-receptor interactions in CellphoneDB database for mouse
+#'
+#' The ligand-receptor interaction database curated in CellphoneDB
+#'
+#' @format A data.frame includes the ligand-receptor interactions
+#' @source \url{https://www.cellphonedb.org/}
+"cpdb_mouse"
