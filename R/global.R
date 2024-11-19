6235bfa65ea1bc8db2893916aea09ef94a511cbf
liuhuoz <liuhuo2012@gmail.com>
2024-11-20 00:13:52 +0800

    add centroid coords related func and doc

--- R/global.R
+++ R/global.R
@@ -1,10 +1,12 @@
 #This file is used to declare global variables used in spWAVE
-#Mainly used in plot functions of ggplot2 aes args.
+#Mainly used in functions of ggplot2 and dplyr because of the tidyverse system.
 utils::globalVariables(
   c("x","y","Ex","Ex.u","Ey","Ey.u",
     "Family","Ligand","Receptor","S2T","Source","Target",
     "lig","rec","p_value",
     "Type2","U","q_net","raw_score","scale_score",
-    "waldStat")
+    "waldStat",
+    "barcode","position","spot_id","uni_id"
+    )
 )
 
