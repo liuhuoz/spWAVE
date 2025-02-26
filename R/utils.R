e315d1e9fa4a91e91d8df39043f3590a9da190f4
liuhuoz <liuhuo2012@gmail.com>
2025-02-26 21:53:08 +0800

    get_coord will catch scale.factor from the first list instead of 'slice1'

--- R/utils.R
+++ R/utils.R
@@ -76,7 +76,7 @@ get_spatial_expr <- function(SrtObj,gene,assay="SCT"){
 get_coordinates <- function(SrtObj){
   if(class(SrtObj@images[[1]]) %in% c("VisiumV1","VisiumV2")){
     coord_info <- Seurat::GetTissueCoordinates(SrtObj,scale=NULL)
-    scale_spot <- SrtObj@images$slice1@scale.factors$spot
+    scale_spot <- SrtObj@images[[1]]@scale.factors$spot
     scale_micrometer <- 65/scale_spot
     coord_info$imagerow <-
       max(coord_info$imagerow) - coord_info$imagerow + min(coord_info$imagerow)
